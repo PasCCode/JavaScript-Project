5a8908948d490ea91d9099d0b317130967334680
PasCCode <pascal.hasko@gmail.com>
2019-08-06 16:45:06 +0200

    Funktion für Bestellungvorgang angelegt

--- YouTube.js
+++ YouTube.js
@@ -1,1 +1,3 @@
-alert("Hello World")
+document.getElementById("button").onclick = function(){
+    document.getElementById("confirm").innerHTML = "Bestellung aufgegeben. Bitte überprüfen Sie Ihre Email für die Bestätigung"
+}
