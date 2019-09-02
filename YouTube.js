131545080707176123fa5bb7179dd3b13a7fba61
PasCCode <pascal.hasko@gmail.com>
2019-09-02 16:32:25 +0200

    Javascript-Datei angepasst

--- YouTube.js
+++ YouTube.js
@@ -1,3 +1,4 @@
 document.getElementById("button").onclick = function(){
     document.getElementById("confirm").innerHTML = "Bestellung aufgegeben. Bitte überprüfen Sie Ihre Email für die Bestätigung";
+    document.getElementById("button").style.display = "none";
 }
