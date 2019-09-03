f2dd921340b7e21704bafc0ac099642d676d91e1
PasCCode <pascal.hasko@gmail.com>
2019-09-03 16:44:18 +0200

    Bestätigungstext erweitert

--- YouTube.js
+++ YouTube.js
@@ -1,4 +1,4 @@
 document.getElementById("button").onclick = function(){
-    document.getElementById("confirm").innerHTML = "Bestellung aufgegeben.";
+    document.getElementById("confirm").innerHTML = "Bestellung aufgegeben. Schauen Sie in Ihrem Postfach nach der Bestätigungsemail";
     document.getElementById("button").style.display = "none";
 }
